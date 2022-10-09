80e3da3e21322d5aa365584dc3c2d09efa082c5d
Silva, Milena (ESI) <milena.silva@adp.com>
2022-10-09 18:40:16 -0300

    implements function readchar

--- WORK/trabalho.asm
+++ WORK/trabalho.asm
@@ -32,10 +32,8 @@ LP:
     cmp ax, cx ;EOF reached?
     jne EOF
 
-    ;PRINT--------------------------------------
     mov al, Buffer
     call printChar
-    ;---------------------------------------------
 
 
     jmp LP ;Read next byte
@@ -45,17 +43,7 @@ EOF:
     int 21h ; chama uma função do MS-DOS
     jc CloseError
 
-READ_FILENAME:
-    mov ah, 0h
-    int 16h ; pra ler caractere do teclado
-    mov ah, 0Eh ; imprime cada caractere incrementando o cursor 
-    mov bh, 0 ; page number (???)
-    mov cx, 1 ; times to print the character
-    int 10h ;calls interruption
-
-    cmp al, 0Dh
-    jz here
-    jmp READ_FILENAME
+    call readChar
 
 here:
 
@@ -75,16 +63,25 @@ printChar proc near
     int 10h ;calls interruption
     ret
 printChar endp
-
-
 ;
 ;--------------------------------------------------------------------
-;Fun��o: Converte um valor HEXA para ASCII-DECIMAL
+;Funcao: Lê char enquanto não lê enter e imprime na tela
 ;Entra:  (A) -> AX -> Valor "Hex" a ser convertido
 ;        (S) -> DS:BX -> Ponteiro para o string de destino
 ;--------------------------------------------------------------------
+readChar proc near
+    mov ah, 0h      ;; seta modo
+    int 16h         ;; pra ler caractere do teclado
+    call printChar  ;; imprime caractere lido
+    cmp al, 0Dh     ;; compara se caractere é enter
+    jz here         ;; se for, condição de parada 
+    call readChar   ;; se não, continua chamadas recursivas
+    ret
+readChar endp
+
+
 BadOpen proc near
-    
+    ret
 BadOpen endp
 
 ;
@@ -94,11 +91,11 @@ BadOpen endp
 ;        (S) -> DS:BX -> Ponteiro para o string de destino
 ;--------------------------------------------------------------------
 ReadError proc near
-    
+    ret    
 ReadError endp
 
 CloseError proc near
-    
+    ret    
 CloseError endp
 
 ;--------------------------------------------------------------------
