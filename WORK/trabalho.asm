5626ce0dceb7f5f512e747323b1ccec5bdda2e57
Silva, Milena (ESI) <milena.silva@adp.com>
2022-10-14 21:25:33 -0300

    validação de chars da frase e organização

--- WORK/trabalho.asm
+++ WORK/trabalho.asm
@@ -4,9 +4,6 @@
 ;       Livro The Art of Assembly Language usado como referência
 ;====================================================================
 
-; nota: eu vou terminar o trabalho e enviar por email ainda de madrugada 
-; mas atualmente eu leio o arquivo, e escrevo num arquivo de saida. :) 
-
 .model small
 
 .STACK
@@ -14,22 +11,30 @@
     hFile       DWORD 0
     pMemory     DWORD 0
     ReadSize    DWORD 0
-    BufferIn    DB 0
-    BufferOut   DB 0
-    FHndlIn     DW 0
-    FHndlOut    DW 0
+    Buffer      DB 0
+    FileHandler DW 0
     endereco    DW 0
     indexTemp   DW 0
-
-.CONST
-    FileNameCte        DB	"texto.txt", 0  
+    charMsg     DB 0
+    charKey     DB 0
     FileNameEntrada    DB 128 DUP(?)
     FileNameSaida      DB 128 DUP(?)
-    Frase              DB 128 DUP(?)
+    Frase              DB 1024 DUP(?)
     
+    temp DW 0
+
+
+.CONST
     CR          equ 0Dh
     LF          equ 0Ah
-    MsgCRL      DB CR, LF, 0
+    MsgCRL      DB CR, LF, 0 ; Quebra de Linha
+
+    ;;;BE CAREFUL
+    MaxArquivo  EQU 65535 ; Tamanho máximo do arquivo de entrada
+    TamArquivo  EQU 0 ; Tamanho real do arquivo de entrada
+    Index       DW 0
+    seExiste    DB 0 ; Indica se caractere já existe no arquivo
+    BufferChar  DB 0 ; Qual caractere está sendo encriptado
 
     ;; interações com o usuário
     inputEntrada DB "Digite o nome do arquivo de entrada: ", CR, LF, 0
@@ -40,8 +45,10 @@
     
     ;; msgs de erro
     ErroLeitura DB "Erros na leitura do arquivo de entrada", CR, LF, 0
+    ErroFechamento DB "Erro no fechamento do arquivo", CR, LF, 0
     ArquivoGde  DB "Arquivo de entrada muito grande (excedeu o tamanho maximo)", CR, LF, 0
     SimbNaoEnc  DB "Nao foi possível encontrar um dos simbolos da frase, no arquivo de entrada fornecido", CR, LF, 0
+    SimbInvalido  DB "Um dos simbolos inseridos na frase e invalido", CR, LF, 0
     ErroArqSaida DB "Erro na criacao do arquivo de saida", CR, LF, 00
 
 .CODE ; Begin code segment
@@ -64,27 +71,26 @@
     lea dx, FilenameEntrada         ; Presume DS points at filename
     int 21h                         ; chama uma função do MS-DOS
     jc BadOpen
-    mov FHndlIn, ax                 ; Save file handle
+    mov FileHandler, ax                 ; Save file handle
 
 LP: 
     mov ah, 3Fh                     ; 3fh é o opcode de readFile no MS-DOS
-    lea dx, BufferIn                ; Ponteiro de buffer
+    lea dx, Buffer                  ; Ponteiro de buffer
     mov cx, 1                       ; Quantos bytes vão ser lidos
-    mov bx, FHndlIn                 ; Get file handle value
+    mov bx, FileHandler                 ; Get file handle value
     int 21h                         ; chama uma função do MS-DOS
     jc ReadError 
     
     cmp ax, cx                      ; EOF encontrado?
     jne EOF
 
-    mov al, BufferIn
+    mov al, Buffer
     call printChar
-    
 
     jmp LP                          ; Lê próximo byte
 EOF: 
     call printEnter
-    mov bx, FHndlIn
+    mov bx, FileHandler
     mov ah, 3Eh                     ; fecha arquivo
     int 21h                         ; chama uma função do MS-DOS
     jc CloseError
@@ -109,7 +115,6 @@ EOF:
     call readString
     call concatenateKRP
 
-
     call printEnter
 ;--------------------------------------------------------------------
 
@@ -120,24 +125,34 @@ EOF:
     lea dx, FileNameSaida ; File to open
     int 21h
     jc BadOpen
-    mov FHndlOut, ax    ; Save output file handle
+    mov FileHandler, ax    ; Save output file handle
     
-    lea si, Sucesso   ;;aponta pro que vai ser escrito no arquivo de saída
+    lea si, SimbInvalido   ;;aponta pro que vai ser escrito no arquivo de saída
 
     loopEscrita:
-        mov bx, FHndlOut    ; file handle
-        mov cx, 1           ; numeros de bytes a escrever
+        mov bx, FileHandler     ; file handle
+        mov cx, 1               ; numeros de bytes a escrever
         mov dx, si 
-        mov ah, 40h         ; opcode pra escrever
+        mov ah, 40h             ; opcode pra escrever
         int 21h
 
-        cmp [si], LF
-        jz _endSucesso
-        inc si
+        cmp [si], LF            ; verifica se o char lido é enter
+        jz _endSucesso          ; se for, pula pro final, 
+        call testaChar
+        inc si                  ; se não, continua lendo
         jmp loopEscrita
+
+    ;; fecha o arquivo
+    call printEnter
+    mov bx, FileHandler
+    mov ah, 3Eh                     ; fecha arquivo
+    int 21h                         ; chama uma função do MS-DOS
+    jc CloseError
 ;--------------------------------------------------------------------
 
 _end:
+.EXIT                               ; Gera exit code
+
     
 _endSucesso:
     lea si, Sucesso
@@ -146,6 +161,23 @@ _endSucesso:
 
 ;; procedimentos
 ;
+;--------------------------------------------------------------------
+;Funcao: verifica se um char está dentro do range pré-determinado de
+;   chars que podem ser encriptados    
+;Entra:  (A) -> [si] -> char a ser testado
+;--------------------------------------------------------------------
+testaChar proc near
+    CMP byte ptr [si], '~' ; maior char admitido
+    JG CharInvalido
+    CMP byte ptr [si], ' ' ; menor char admitido
+    JL carriageReturn
+    ret
+carriageReturn:
+    CMP byte ptr [si], CR ; testa se é carriage return
+    JNE CharInvalido
+testaChar endp
+
+
 ;--------------------------------------------------------------------
 ;Funcao: imprime um char na tela
 ;Entra:  (A) -> AL -> char a ser impresso
@@ -177,31 +209,8 @@ read:
     jmp read   ;; se não, continua chamadas recursivas
 readRet:
     ret
+
 readString endp
-;
-;--------------------------------------------------------------------
-;Funcao: imprime erro de leitura 
-;--------------------------------------------------------------------
-BadOpen proc near
-    lea si, ErroLeitura
-    call printMsg
-    jmp _end
-    ret
-BadOpen endp
-
-ReadError proc near
-    lea si, ErroLeitura
-    call printMsg
-    jmp _end
-    ret    
-ReadError endp
-
-CloseError proc near
-    lea si, ErroLeitura
-    call printMsg
-    jmp _end
-    ret    
-CloseError endp
 ;
 ;--------------------------------------------------------------------
 ;Funcaoo: Concatena .txt num filename
@@ -218,7 +227,6 @@ concatenateTXT proc near
     mov [si], 't'
     ret
 concatenateTXT endp
-
 ;
 ;--------------------------------------------------------------------
 ;Funcaoo: Concatena .krp num filename
@@ -235,7 +243,6 @@ concatenateKRP proc near
     mov [si], 'p'
     ret
 concatenateKRP endp
-
 
 ;--------------------------------------------------------------------
 ;Funcao: Printa uma mensagem
@@ -262,7 +269,53 @@ printEnter proc near
     call printChar
     ret
 printEnter endp
+;
+;--------------------------------------------------------------------
+;Funcao: imprime erro de leitura 
+;--------------------------------------------------------------------
+BadOpen proc near
+    call printEnter
+    lea si, ErroLeitura
+    call printMsg
+    jmp _end
+    ret
+BadOpen endp
 
+ReadError proc near
+    call printEnter
+    lea si, ErroLeitura
+    call printMsg
+    jmp _end
+    ret    
+ReadError endp
+
+CloseError proc near
+    call printEnter
+    lea si, ErroFechamento
+    call printMsg
+    jmp _end
+    ret    
+CloseError endp
+;
+;--------------------------------------------------------------------
+;Funcao: imprime erro referente a frase 
+;--------------------------------------------------------------------
+CharNaoEnc proc near
+    call printEnter
+    lea si, SimbNaoEnc
+    call printMsg
+    jmp _end
+    ret    
+CharNaoEnc endp
+
+CharInvalido proc near
+    call printEnter
+    lea si, SimbInvalido
+    call printMsg
+    jmp _end
+    ret    
+CharInvalido endp
+;
 ;--------------------------------------------------------------------
 	end
 ;--------------------------------------------------------------------
