8bda77fb1da2a034aa8fdb09040d02b773c7ca56
Silva, Milena (ESI) <milena.silva@adp.com>
2022-10-18 19:11:41 -0300

    done?

--- WORK/trabalho.asm
+++ WORK/trabalho.asm
@@ -6,26 +6,32 @@
 
 .model small
 
-.STACK
+.STACK 100h
+
+
 .DATA
     Buffer      DB 0 ; buffer dos char 
     FileHandler DW 0 ; file handler dos arquivos
-    endereco    DW 0
-    indexTemp   DW 0
-    charMsg     DB 0
-    charKey     DB 0
     FileNameEntrada    DB 128 DUP(?)    ; nome do arquivo de entrada
     FileNameSaida      DB 128 DUP(?)    ; nome do arquivo de entrada
     Frase              DB 1024 DUP(?)   ; frase lida pra ser encriptada
-    zero               DB 0
     ChaveLida          DB 256 DUP(?)    ; chave lida do arquivo de entrada
     bufferCripto    DB 0             ; buffer pra criptografia
-    numCripto DB 0
     temp DW 0
-    temp2 DB 0
-    indexArquivo DB 0
+    indexArquivo DW 0
     strResult DB 16 DUP(?) ; string pra conversão de numero pra ascii
     tempIndex DW 0 ; variavel pra salvar index que tá percorrendo frase pq vai ser usado por outro proc
+    Quociente DB 132
+    Resto DB 0
+    NumAlgarismos DB 0
+    fileHandleEscrita DW 0
+    charEscrita DB 0
+    tempWord DW 0
+    outroFileHandle DW 0
+    numeroString DB 256 DUP(0)
+    zeroFinal DB 0
+    tamFrase DW 0
+    tamArquivoEntrada DW 0
 
 .CONST
     CR          equ 0Dh         ; Código ASCII de CR
@@ -33,22 +39,17 @@
     MsgCRL      DB CR, LF, 0    ; Quebra de Linha
     MaxFrase    DB 100
 
-    ;;;BE CAREFUL
-    MaxArquivo  EQU 65535 ; Tamanho máximo do arquivo de entrada
-    TamArquivo  EQU 0 ; Tamanho real do arquivo de entrada
-    Index       DW 0
-    seExiste    DB 0 ; Indica se caractere já existe no arquivo
-    BufferChar  DB 0 ; Qual caractere está sendo encriptado
-
     ;; interações com o usuário
     inputEntrada DB "Digite o nome do arquivo de entrada: ", CR, LF, 0
     inputSaida  DB "Digite o nome do arquivo de saida: ", CR, LF, 0
     inputFrase  DB "Digite a frase a ser criptografada: ", CR, LF, 0
 
     Sucesso     DB "Processamento realizado sem erros", CR, LF, 0
-    
+    msgTamFrase DB "Tamanho da frase (em bytes): ", CR, LF, 0
+    msgTamArquivo DB "Tamanho do arquivo de entrada (em bytes): ", CR, LF, 0
+
     ;; msgs de erro
-    ErroLeitura DB "Erros na leitura do arquivo de entrada", CR, LF, 0
+    ErroLeitura DB "Erros na abertura do arquivo", CR, LF, 0
     ErroFechamento DB "Erro no fechamento do arquivo", CR, LF, 0
     ArquivoGde  DB "Arquivo de entrada muito grande (excedeu o tamanho maximo)", CR, LF, 0
     SimbNaoEnc  DB "Nao foi possivel encontrar um dos simbolos da frase, no arquivo de entrada fornecido", CR, LF, 0
@@ -57,8 +58,10 @@
     FraseMuitoGrande DB "Frase maior que o tamanho permitido", CR, LF, 00
     ErroFraseVazia DB "Frase vazia", CR, LF, 00
 
+    ConstDez DB 10
 .CODE ; Begin code segment
 .STARTUP ; Generate start-up code
+;--------------------------------------------------------------------------------cut
 
 ;---interação com usuário (arq entrada)------------------------------ 
     lea si, inputEntrada
@@ -77,7 +80,6 @@
 
     lea si, ChaveLida
     call converteLower
-
 ;--------------------------------------------------------------------
 
 ;---interação com usuário (frase)------------------------------- 
@@ -92,7 +94,6 @@
     lea si, Frase
     call validaTamFrase
     call printEnter
-
 ;--------------------------------------------------------------------
 
 ;---interação com usuário (arq. saída)------------------------------- 
@@ -107,16 +108,14 @@
     call printEnter
 ;--------------------------------------------------------------------
 
+;criptografia--------------------------------------------------------
 
- ;; abre o arquivo
-    mov ah, 3Ch         ; Create file call
-    mov cx, 0           ; Normal file attributes
-    lea dx, FileNameSaida ; File to open
-    int 21h             ; chama uma função do ms-dos
-    jc BadOpen
+    ; cria arquivo pra escrita
+    lea ax, FileNameSaida
+    call criaArquivoParaEscrita
     mov FileHandler, ax    ; Save output file handle
 
-;criptografia--------------------------------------------------------
+
     lea bp, ChaveLida   ; chave lida 
     mov di, 0           ; indica a posição no arquivo
     mov indexArquivo, 0
@@ -126,8 +125,15 @@
         cmp byte ptr [si], 0    ; verifica se é o fim da frase
         je fimCripto            ; se for, pula pro fim do loop
 
+        ;------verifica se o char está dentro do range que deve ser criptografado
+        cmp byte ptr [si], ' '
+        jle vaiProProximo
+
+        cmp byte ptr [si], '~'
+        jg vaiProProximo
+        ;-------------------------------------------------------------------
     loopArquivo:
-        mov al, byte ptr [bp+di+0]  ; coloca bp+di em al para poder comparar
+        mov al, byte ptr [bp+di+0]  ; coloca bp+di(chave) em al para poder comparar
         cmp al, byte ptr [si]       ; compara com a frase
         je criptografa              ; se igual, pula pra criptografia
         
@@ -136,92 +142,233 @@
         cmp byte ptr [bp+di+0], 0
         je CharNaoEnc
         
-        inc di
-        inc indexArquivo
+        inc di ; pula para próximo char da chave
+        inc indexArquivo ; guarda posição
         
         jmp loopArquivo
 
     criptografa: 
-        mov ah, 0
-        mov al, indexArquivo
-        
-        call converteNumPraASCII    
+        mov ax, indexArquivo ; bota em ax pra dividir
+        mov Quociente, al
+        NumPraASCIIDivisaoLoop: ;divisão pra separar cada caractere do numero
+            mov al, Quociente
+            mov ah, 0
 
-        mov [bp+di+0], '×'
-        mov indexArquivo, 0
+            div ConstDez
+            mov Quociente, al
+            mov Resto, ah
+
+            add Resto, 48 ;soma pra converter pro código ascii
+            mov al, Resto
+            mov ah, 0
+            push ax ; usa a pilha pois o número fica invertido
+
+            inc numAlgarismos
+
+            cmp Quociente, 0
+            je loopEscreveNumero
+            jmp NumPraASCIIDivisaoLoop
+
+    loopEscreveNumero:
+        lea di, numeroString
+    loopdoloop:
+        cmp numAlgarismos, 0
+        je fimEscreveNumero
+
+        pop bx ; puxa o algarismo da pilha
+
+        mov byte ptr [di], bl ; move o caractere pra numeroString
+
+        dec numAlgarismos
+        inc di
+
+        jmp loopdoloop
+
+    fimEscreveNumero:
+        mov byte ptr [di], 0 ; marca final
+
+        lea di, numeroString ; bota o ponteiro pra começo de numeroString
+    escreveNumeroLoop:
+        cmp byte ptr [di], 0 ; se final, pula pra terminou de escrever
+        je terminouDeEscrever
+
+        ; se não, escreve no arquivo
+        mov ax, FileHandler
+        mov bl, byte ptr [di]
+        call escreveCharNoArquivo
+        inc di
+        jmp escreveNumeroLoop
+
+    terminouDeEscrever: ;imprime quebra de linha 
+        mov ax, FileHandler
+        mov bl, CR
+        call escreveCharNoArquivo
+
+        mov ax, FileHandler
+        mov bl, LF
+        call escreveCharNoArquivo
+
         mov di, 0
-        inc si
-        cmp [si], 0
-        jnz loopCriptografia
+        
+    loopinc: ; move o valor de indexArquivo de volta pra di
+        cmp indexArquivo, 0
+        je fiminc
+        inc di
+        dec indexArquivo
+        jmp loopinc
+
+    fiminc:
+        mov byte ptr [bp+di+0], ' ' ; substitui char já usado como chave por ' ' pra ser ignorado
+        
+        ; move ponteiros da chave pro inicio
+        mov indexArquivo, 0 
+        mov di, 0
+    vaiProProximo:
+        inc si ; incrementa ponteiro do char da frase
+        jmp loopCriptografia
 
     fimCripto:
+        ;; fecha o arquivo
+        call Escreve0000        
+        
+        call printEnter
 
+        lea si, Sucesso
+        call printMsg
+
+        call printEnter
+
+        lea si, msgTamFrase
+        call printMsg
+
+        call printEnter
+
+        mov ax, tamFrase
+        call printNumero
+
+        call printEnter
+
+        lea si, msgTamArquivo
+        call printMsg
+
+        call printEnter
+
+        mov ax, tamArquivoEntrada
+        call printNumero
+
+        mov bx, FileHandler
+        mov ah, 3Eh                     ; fecha arquivo
+        int 21h                         ; chama uma função do MS-DOS
+        jc CloseError
 ;--------------------------------------------------------------------
-
-;---escrita no arquivo----------------------------------------------- 
-   
-    
-
-    ;; fecha o arquivo
-    call printEnter
-    mov bx, FileHandler
-    mov ah, 3Eh                     ; fecha arquivo
-    int 21h                         ; chama uma função do MS-DOS
-    jc CloseError
+.EXIT
 ;--------------------------------------------------------------------
 
 _end:
 .EXIT                               ; Gera exit code
-    
-_endSucesso:
-    lea si, Sucesso                 ; indica a mensagem que vai ser impressa 
-    call printMsg
-.EXIT                               ; Gera exit code
 
 ;; procedimentos
 ;--------------------------------------------------------------------
-;Funcao: converte numero pra ASCII
-;Entra:  (A) -> al -> numero a ser convertido
+;Funcao: Lê arquivo de entrada 
+;Entra:  (A) -> Si -> ponteiro pra onde vai ser salva a chave
 ;--------------------------------------------------------------------
-converteNumPraASCII proc near
-    mov dx, 0
-    mov bufferCripto, dl
-    mov strResult, dl
+leituraArquivo proc near
+    lea ax, FilenameEntrada
+    call abreArquivoParaLeitura
+    
+    mov FileHandler, ax             ; Save file handle
 
-    lea bx, strResult
-    mov cl, 10
-   
+    LP: 
+        mov ax, FileHandler
+        lea bx, Buffer
+        call leCharDoArquivo
+        
+        cmp ax, cx                      ; EOF encontrado?
+        jne EOF
 
-    lpCtAscii:
-        mov cl, 10
-        div cl ;ax = numero /10, ah = resto
-        mov temp, ax
-        mov tempIndex, bx
+        mov al, Buffer
+        mov [si], al                    ; guarda o char lido na Chave
+        inc si
 
-        add al, 48
-        mov bufferCripto, al
+        jmp LP                          ; Lê próximo byte
+    EOF: 
+        mov [si], 0                     ; concatena 0 no fim da chave
+        call printEnter
+        
+        mov ax, fileHandler
+        call fechaArquivo
 
-        mov bx, FileHandler     ; file handle
-        mov cx, 1               ; numeros de bytes a escrever
-        lea dx, bufferCripto
-        mov ah, 40h             ; opcode pra escrever
-        int 21h                 ; chama uma função do MS-DOS
+        ret 
+leituraArquivo endp
 
-        mov ax, temp
-        add ah, 48
-        mov bufferCripto, ah
+;Tendo um fileHandle em ax e o endereço de um buffer em bx,
+;lê um byte do arquivo e guarda no buffer
+leCharDoArquivo proc near
+    mov tempWord, bx
+    mov dx, tempWord ;buffer em dx
 
-        mov bx, FileHandler     ; file handle
-        mov cx, 1               ; numeros de bytes a escrever
-        lea dx, bufferCripto
-        mov ah, 40h             ; opcode pra escrever
-        int 21h                 ; chama uma função do MS-DOS
+    mov tempWord, ax
+    mov bx, tempWord ;handle em bx
 
-        mov ax, temp
-        mov bx, tempIndex
+    mov cx, 1                       ; Quantos bytes vão ser lidos
+
+    mov ah, 3Fh                     ; 3fh é o opcode de readFile no MS-DOS
+    int 21h                         ; chama uma função do MS-DOS
+    inc tamArquivoEntrada
+
+    jc ReadError 
     ret
+leCharDoArquivo endp
 
-converteNumPraASCII endp
+;tendo uma file handle em ax (com o arquivo aberto para escrita) e um caractere no bl, 
+;escreve o caractere no arquivo
+escreveCharNoArquivo proc near
+    mov fileHandleEscrita, ax
+    mov charEscrita, bl
+
+    mov bx, fileHandleEscrita     ; file handle
+    mov cx, 1               ; numeros de bytes a escrever
+    lea dx, charEscrita
+    mov ah, 40h             ; opcode pra escrever
+    int 21h                 ; chama uma função do MS-DOS
+
+    ret
+escreveCharNoArquivo endp
+
+;tendo um ponteiro para o endereço de arquivo em ax, cria este arquivo e
+;retorna o file handle em ax
+criaArquivoParaEscrita proc near
+    mov tempWord, ax
+    mov dx, tempWord
+
+    mov cx, 0           ; Normal file attributes
+    mov ah, 3Ch         ; Create file call
+    int 21h             ; chama uma função do ms-dos
+    jc BadOpen
+    ret
+criaArquivoParaEscrita endp
+
+;tendo um ponteiro para o endereço de arquivo em ax, abre este arquivo e
+;retorna o file handle em ax
+abreArquivoParaLeitura proc near
+    mov tempWord, ax
+    mov dx, tempWord
+    mov ah, 3Dh                     ; Open the file
+    mov al, 0                       ; Open for reading
+    int 21h                         ; chama uma função do MS-DOS
+    jc BadOpen
+    ret
+abreArquivoParaLeitura endp
+
+;tendo um fileHandle em ax, fecha este arquivo
+fechaArquivo proc near
+    mov tempWord, ax
+    mov bx, tempWord
+    mov ah, 3Eh                     ; fecha arquivo
+    int 21h                         ; chama uma função do MS-DOS
+    jc CloseError
+    ret
+fechaArquivo endp
 
 ;--------------------------------------------------------------------
 ;Funcao: valida tamanho da frase
@@ -233,6 +380,7 @@ validaTamFrase proc near
         cmp byte ptr [si], 0
         je eofTAM
         inc si
+        inc tamFrase
 
         inc bl
         cmp bl, 100
@@ -243,7 +391,6 @@ validaTamFrase proc near
         jz FraseVazia
         ret
 validaTamFrase endp
-
 ;--------------------------------------------------------------------
 ;Funcao: converte uma string em caixa baixa
 ;Entra:  (A) -> [si] -> string a ser convertido
@@ -279,7 +426,6 @@ testaChar proc near
         CMP byte ptr [si], CR ; testa se é carriage return
         JNE CharInvalido
 testaChar endp
-
 ;--------------------------------------------------------------------
 ;Funcao: imprime um char na tela
 ;Entra:  (A) -> AL -> char a ser impresso
@@ -313,45 +459,6 @@ readString proc near
         ret
 
 readString endp
-;
-;--------------------------------------------------------------------
-;Funcao: Lê arquivo de entrada 
-;Entra:  (A) -> Si -> ponteiro pra onde vai ser salva a chave
-;--------------------------------------------------------------------
-leituraArquivo proc near
-    mov ah, 3Dh                     ; Open the file
-    mov al, 0                       ; Open for reading
-    lea dx, FilenameEntrada         ; Presume DS points at filename
-    int 21h                         ; chama uma função do MS-DOS
-    jc BadOpen
-    mov FileHandler, ax             ; Save file handle
-
-    LP: 
-        mov ah, 3Fh                     ; 3fh é o opcode de readFile no MS-DOS
-        lea dx, Buffer                  ; Ponteiro de buffer
-        mov cx, 1                       ; Quantos bytes vão ser lidos
-        mov bx, FileHandler             ; Get file handle value
-        int 21h                         ; chama uma função do MS-DOS
-        jc ReadError 
-        
-        cmp ax, cx                      ; EOF encontrado?
-        jne EOF
-
-        mov al, Buffer
-        mov [si], al                    ; guarda o char lido na Chave
-        inc si
-
-        jmp LP                          ; Lê próximo byte
-    EOF: 
-        mov [si], 0                     ; concatena 0 no fim da chave
-        call printEnter
-        mov bx, FileHandler
-        mov ah, 3Eh                     ; fecha arquivo
-        int 21h                         ; chama uma função do MS-DOS
-        jc CloseError
-
-        ret 
-leituraArquivo endp
 ;
 ;--------------------------------------------------------------------
 ;Funcaoo: Concatena .txt num filename
@@ -441,7 +548,7 @@ CloseError proc near
 CloseError endp
 ;
 ;--------------------------------------------------------------------
-;Funcao: imprime erro referente a frase 
+;Funcao: imprime erros referente a frase 
 ;--------------------------------------------------------------------
 CharNaoEnc proc near
     call printEnter
@@ -475,7 +582,59 @@ FraseVazia proc near
     ret
 FraseVazia endp
 
+;--------------------------------------------------------------------
+;Funcao: escreve '0000' num arquivo
+;--------------------------------------------------------------------
+Escreve0000 proc near
+    loop0000:
+        mov ax, FileHandler
+        mov bl, '0'
+        call escreveCharNoArquivo
+        inc zeroFinal
+        cmp zeroFinal, 4
+        jne loop0000
+    ret
+Escreve0000 endp
 
+;--------------------------------------------------------------------
+;Funcao: printa um numero na tela
+;Entra:  (A) -> ax -> numero a ser printado
+;--------------------------------------------------------------------
+printNumero proc near
+    mov Quociente, al
+    NumPraASCIIDivisaoLoop2:
+            mov al, Quociente
+            mov ah, 0
+
+            div ConstDez
+            mov Quociente, al
+            mov Resto, ah
+
+            add Resto, 48
+            mov al, Resto
+            mov ah, 0
+            push ax
+
+            inc numAlgarismos
+
+            cmp Quociente, 0
+            je loopEscreveNumero2
+            jmp NumPraASCIIDivisaoLoop2
+
+    loopEscreveNumero2:
+        cmp numAlgarismos, 0
+        je fimEscreveNumero2
+
+        pop ax
+        call printChar
+
+        dec numAlgarismos
+
+        jmp loopEscreveNumero2
+
+    fimEscreveNumero2:
+    ret
+printNumero endp
 
 ;
 ;--------------------------------------------------------------------
